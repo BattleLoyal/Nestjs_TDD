32016dd10e22fac9441b4782d9b0a9ddc812ae84
BattleLoyal <dbslxl12345@gmail.com>
2024-09-15 21:25:04 +0900

    TDD Chapter3 - Equals method

--- src/app.money.spec.ts
+++ src/app.money.spec.ts
@@ -11,6 +11,11 @@ class Dollar {
 	times(multiplier:number):Dollar {
 		return new Dollar(this.amount * multiplier);
 	}
+
+	equals(object:Object):boolean {
+		let dollar:Dollar = object as Dollar;
+		return this.amount == dollar.amount;
+	}
 }
 
 describe('DollarTest', () => {
@@ -18,8 +23,8 @@ describe('DollarTest', () => {
 		it('multiplydollar', () => {
 			let num:number = 10;
         	let five:Dollar = new Dollar(5);
-			five.times(2);
-			expect(num).toBe(five.amount);
+			let ten:Dollar = five.times(2);
+			expect(num).toBe(ten.amount);
 		});
 
 		it('testMultipication', () => {
@@ -28,6 +33,11 @@ describe('DollarTest', () => {
 			expect(10).toBe(product.amount);
 			product = five.times(3);
 			expect(15).toBe(product.amount);
+		});
+
+		it('testEquality', () => {
+			expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
+			expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
 		});
 	});
 });
